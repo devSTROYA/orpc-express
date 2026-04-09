ce24c7a3b5e24e157e04c8de16e856b2c9f813dc
Mudzia Hutama <mudzia.hutama@gmail.com>
2026-04-09 10:53:05 +0700

    feat: make router depend to controller

--- src/features/song/routers/song.router.ts
+++ src/features/song/routers/song.router.ts
@@ -1,13 +1,15 @@
+import { SongController } from '../controllers';
 import { SongRepository } from '../repositories';
 import { SongService } from '../services';
 
-const songRepository = new SongRepository();
-const songService = new SongService(songRepository);
+const repository = new SongRepository();
+const service = new SongService(repository);
+const controller = new SongController(service);
 
 export const songRouter = {
-  list: songService.list.route({ path: '/songs', method: 'GET' }),
-  find: songService.find.route({ path: '/songs/:id', method: 'GET' }),
-  create: songService.create.route({ path: '/songs', method: 'POST' }),
-  update: songService.update.route({ path: '/songs/:id', method: 'PUT' }),
-  delete: songService.delete.route({ path: '/songs/:id', method: 'DELETE' }),
+  list: controller.listSongs().route({ path: '/songs', method: 'GET' }),
+  find: controller.findSong().route({ path: '/songs/:id', method: 'GET' }),
+  create: controller.createSong().route({ path: '/songs', method: 'POST' }),
+  update: controller.updateSong().route({ path: '/songs/:id', method: 'PUT' }),
+  delete: controller.deleteSong().route({ path: '/songs/:id', method: 'DELETE' }),
 };
