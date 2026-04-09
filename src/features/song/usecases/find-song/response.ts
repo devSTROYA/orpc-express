b69d3350c7c74da8ff9cc9e7013ef2fa75bf714c
Mudzia Hutama <mudzia.hutama@gmail.com>
2026-04-09 10:50:16 +0700

    feat: remove nullable from find song response

--- src/features/song/usecases/find-song/response.ts
+++ src/features/song/usecases/find-song/response.ts
@@ -1,8 +1,6 @@
 import { z } from 'zod';
 import { SongSchema } from '../../schemas';
 
-export const Schema = z.object({
-  data: SongSchema,
-});
+export const Schema = z.object({ data: SongSchema });
 
 export type Response = z.infer<typeof Schema>;
