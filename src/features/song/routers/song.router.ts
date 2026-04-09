bb1627210a017c16afa9d449820f97d748342acb
Mudzia Hutama <mudzia.hutama@gmail.com>
2026-04-09 13:58:19 +0700

    feat: add endpoint summary

--- src/features/song/routers/song.router.ts
+++ src/features/song/routers/song.router.ts
@@ -7,9 +7,13 @@ const service = new SongService(repository);
 const controller = new SongController(service);
 
 export const songRouter = {
-  list: controller.listSongs().route({ path: '/songs', method: 'GET' }),
-  find: controller.findSong().route({ path: '/songs/:id', method: 'GET' }),
-  create: controller.createSong().route({ path: '/songs', method: 'POST' }),
-  update: controller.updateSong().route({ path: '/songs/:id', method: 'PUT' }),
-  delete: controller.deleteSong().route({ path: '/songs/:id', method: 'DELETE' }),
+  list: controller.listSongs().route({ path: '/songs', method: 'GET', summary: 'List songs' }),
+  find: controller.findSong().route({ path: '/songs/:id', method: 'GET', summary: 'Find song' }),
+  create: controller.createSong().route({ path: '/songs', method: 'POST', summary: 'Create song' }),
+  update: controller
+    .updateSong()
+    .route({ path: '/songs/:id', method: 'PUT', summary: 'Update song' }),
+  delete: controller
+    .deleteSong()
+    .route({ path: '/songs/:id', method: 'DELETE', summary: 'Delete song' }),
 };
